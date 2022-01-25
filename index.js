264c0a8274bccee18b5a9bd362964c8a8d8aa776
Damola Adewunmi <damolasd@gmail.com>
2022-01-25 22:05:04 +0100

    changed app from socket mode to use request urls

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const { App } = require("@slack/bolt");
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   token: process.env.SLACK_BOT_TOKEN,
-  socketMode: true,
-  appToken: process.env.SEREN_TOKEN,
+  // socketMode: true,
+  // appToken: process.env.SEREN_TOKEN,
 });
 const token = process.env.SLACK_BOT_TOKEN;
 
